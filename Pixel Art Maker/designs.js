135117de0f77bfbc0870f06dcdceed7aeaeb71e9
mitush <mitushkushmith1@gmail.com>
2018-05-05 19:10:52 +0530

    Modified JavaScript for Color

--- Pixel Art Maker/designs.js
+++ Pixel Art Maker/designs.js
@@ -8,21 +8,31 @@ $('#inp_submit').click(function(e){
 });
 function makeGrid() {
 var r = $('#inputHeight').val();
-console.log(r);
 var c = $('#inputWeight').val();
 var tab = $('#pixelCanvas');
+var color = $('#colorPicker');
+console.log(color.val());
 tab.html("");
 //alert("inside func");
 for(var i=0 ; i<r ; i++)
 {	  
 	// row = tab.insertRow(i);
 	var row = $("<tr>");
+	tab.append(row);
 	//alert("inside func1");
 	for(var j=0 ; j<c ; j++){
 		// cell = row.insertCell(j);
 		row.append($("<td>"));
+		$('td').attr("class","cell");
 	}
 	tab.append(row);
 }
+// alert("inside functest");
+$('.cell').click(function (event){
+	var c = color.val();
+	//alert("INside2");
+	console.log('color is '+c);
+	$(event.target).css('background-color',c);
+});
 
 }
